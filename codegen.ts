f2d8741aad811fd1a672421dc33ae9aa964fa4dc
ts-geek43 <manish.prajapati@techstaunch.com>
2023-04-20 15:30:30 +0530

    schema path changed

--- codegen.ts
+++ codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "schema.graphql",
+  schema: "*.graphql",
   generates: {
     "generated.ts": {
       plugins: ["typescript", "typescript-resolvers"]
